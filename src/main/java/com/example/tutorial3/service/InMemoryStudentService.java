a66e406292f2589b535cc9000a805260b9b389fa
NovalinaHutabarat <novalinahutabarat93@gmail.com>
2018-03-03 09:07:54 +0000

    first commit

---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,39 @@
+package com.example.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService{
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		for(int i=0; i<studentList.size(); i++) {
+			if(npm.equalsIgnoreCase(studentList.get(i).getNpm())) {
+				return studentList.get(i);
+			}	
+		}
+		return null;
+	}
+	
+	@Override
+	public List<StudentModel> selectAllStudents(){
+		return studentList;
+	}
+	
+	@Override
+	public void addStudent(StudentModel student) {
+		studentList.add(student);
+	}
+	
+	@Override
+	public void delete(String npm) {
+		for(int i=0; i<studentList.size(); i++) {
+			if(npm.equalsIgnoreCase(studentList.get(i).getNpm())) {
+				studentList.remove(i);
+			}	
+		}
+		
+	}
+}
